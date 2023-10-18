876de099b301a6a164b6ad070e5bd035022a792b
jorgeduje <jorgeduje4@gmail.com>
2023-10-18 20:16:39 -0300

    variables tipos de datos y operadores

---
+++ variables.js
@@ -0,0 +1,91 @@
+
+// ANCHOR ---> VARIABLES ---> cajas que guardan algo
+
+// let - const
+
+
+let nombreDeUsuario = "pepito"
+
+const apellido = "perez"
+
+let edad = 22
+
+console.log( nombreDeUsuario )
+
+nombreDeUsuario = "Maria"
+
+console.log( nombreDeUsuario)
+
+// apellido = "Gonzalez"
+
+// console.log(apellido)
+
+// ANCHOR TIPOS DE DATOS 
+
+// strings ---> cadenas de texto
+
+// let frase = 'hola "como" estas?'
+let frase = "hola 'como' estis?"
+
+
+// numbers
+let altura = 176.4
+
+let numeroDeTelefono = "5412312565"
+let numero_de_telefono = "5412312565"
+
+// let numerodetelefono = "5412312565"
+
+console.log( typeof altura )
+console.log( typeof numeroDeTelefono )
+
+
+// booleanos - booleans ---> true o false
+let usuarioLogueado = false
+
+// undefined 
+
+let nombre
+
+console.log( nombre )
+// null 
+
+
+// NaN --> not a number
+let a = 2 
+let b = "hola"
+let resultado = a * b 
+
+console.log(resultado)
+
+// ANCHOR --->  operadores 
+
+// matematicos
+
+let x =13
+let y = 5
+
+console.log( x + y ) 
+console.log( x - y ) 
+console.log( x * y ) 
+console.log( x / y )
+
+console.log( x % y ) // ---> resto de la division
+console.log( x % 2 ) // ---> === 0
+
+let saludo = "como estas"
+let usuario = "pepe"
+
+// concatenacion 
+console.log( "hola "  + usuario + " " + saludo )
+
+// template literals (backsticks)  ---> interpolar
+
+console.log( `hola ${usuario} ${saludo}` )
+
+
+
+// comparacion
+
+// logicos 
+
