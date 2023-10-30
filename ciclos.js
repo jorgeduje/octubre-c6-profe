269a0decdd6c96e566eab84c2c9f391fb367e1c2
jorgeduje <jorgeduje4@gmail.com>
2023-10-30 20:16:34 -0300

    se trabaja con ciclo for

---
+++ ciclos.js
@@ -0,0 +1,66 @@
+
+// ciclos -- bucles -- loop
+// for - while - do while
+
+// inicializacion - condicion - modificador
+
+const probandoFor = ()=>{
+
+    for( let i = 1 ; i <= 5 ; i = i + 1 ){
+
+        console.log(i)
+        console.log("hola")
+        
+    }
+   
+    console.log("me ejecute")
+
+
+}
+
+probandoFor()
+
+
+// 1 - 5 --->  1+ 2 + 3+ 4+ 5 = 15
+// 1 - x --->  x+ x+ x+ x+ x+ = n 
+// x - z --->  x+ x+ x+ x+ x+ = n 
+
+// pero solo quiero los pares
+// x - z --->  x+ x+ x+ x+ x+ = n 
+
+const calcularSumatoria = (  numInicial, numFinal )=>{
+
+    let acumulador = 0 // 6
+
+    for( let i = numInicial; i <= numFinal ; i++ ){
+
+        if( i % 2 === 0 ){
+            acumulador += i
+        }
+
+    }
+
+    return acumulador
+
+}
+
+let res = calcularSumatoria(1,5)
+console.log(res)
+
+
+
+// let x = 20 
+// x = x + 1
+// x += 1 
+// x++
+// x--
+
+
+// let num = 54
+
+// // si quiero que mi comparacion de true cuando el num es par
+// console.log( num % 2 === 0  )
+
+// // si quiero que mi comparacion de true cuando el num es impar
+// console.log( num % 2 !== 0 )
+
--- condicionales.js
+++ condicionales.js
@@ -1,4 +1,3 @@
-
 // ANCHOR CONDICIONALES ---> if / ternario / switch-case
 
 // UNA FUNCION QUE RETORNE TRUE  SI EL USUARIO TIENE PERMISO O FALSE SI NO LO TIENE
@@ -6,49 +5,91 @@
 // si el rol ---> "admin" si tiene permisos
 // si el rol ---> no es "admin" no tiene permiso
 
-const determinarPermisos = rolDelUsuario => {
+const determinarPermisos = (rolDelUsuario) => {
+  //    if( rolDelUsuario === "admin" ){
+  //     // NUEVO BLOQUE
+  //         return true
+  //    }else{
+  //     // BLOQUE
+  //         return false
+  //    }
 
-//    if( rolDelUsuario === "admin" ){
-//     // NUEVO BLOQUE
-//         return true
-//    }else{
-//     // BLOQUE
-//         return false
-//    }
+  // condicion ? lo verdadero : lo falso
 
-   // condicion ? lo verdadero : lo falso
+  let permiso =
+    rolDelUsuario === "admin" ? "Tenes acceso a todo" : "No tenes acceso nada";
+  return permiso;
 
-   let permiso = rolDelUsuario === "admin" ? "Tenes acceso a todo" : "No tenes acceso nada" 
-    return permiso
+  //    return rolDelUsuario === "admin" ? "Tenes acceso a todo" : "No tenes acceso nada"
+};
 
-//    return rolDelUsuario === "admin" ? "Tenes acceso a todo" : "No tenes acceso nada" 
-
-}
-
-
-let tienePermiso = determinarPermisos("usuario")
-console.log(tienePermiso)
-
+let tienePermiso = determinarPermisos("usuario");
+console.log(tienePermiso);
 
 // superAdmin --> "tenes acceso a todo"
 // admin --> "tenes acceso a casi todo"
 // usuario --> "no tenes acceso"
 
-const determinar = rol => {
+const determinar = (rol) => {
+  let permisoFinal = undefined;
 
-    let permisoFinal = undefined
+  if (rol === "superAdmin") {
+    permisoFinal = "tenes acceso a todo";
+  } else if (rol === "admin") {
+    permisoFinal = "tenes acceso a casi todo";
+  } else {
+    permisoFinal = "no tenes acceso";
+  }
 
-    if(rol === "superAdmin"){
-       permisoFinal =  "tenes acceso a todo"
-    }else if(rol === "admin"){
-        permisoFinal = "tenes acceso a casi todo"
-    }else{
-        permisoFinal = "no tenes acceso"
-    }
-
-    return permisoFinal
-
-}
+  return permisoFinal;
+};
 
 // let x  = determinar("superAdmin")
 // console.log(x)
+
+// SWITCH CASE
+
+// impuesto inicial ---> 200
+
+// audi ---> 100
+// vw ---> 50
+// fiat ---> 25
+// reanault ---> 30
+// volvo ---> 40
+// ford ---> 45
+
+// if( marca === "audi"){
+//     // impuesto = impuesto + 100
+//     impuesto += 100
+// }else if(marca === "vw"){
+//     impuesto += 50
+// }else if( marca === "fiat"){
+//     impuesto += 25
+// }else if(marca === "volvo"){
+//     impuesto += 40
+// }
+
+const calcularImpuesto = (marca) => {
+  let impuesto = 200;
+
+  switch (marca) {
+    case "audi":
+      impuesto += 100;
+      break
+    case "vw":
+      impuesto += 50;
+      break
+    case "fiat":
+      impuesto += 25;
+      break
+    default:
+        impuesto = "Su auto esta excento!"
+  }
+
+  
+  return impuesto
+
+};
+
+let resultadoImpuesto = calcularImpuesto("fiat");
+console.log(resultadoImpuesto)
